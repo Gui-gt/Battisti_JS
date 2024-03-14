b3f88f49e68de709c8f0822aae378286c779e898
Gui-gt <30755guilherme@gmail.com>
2024-03-14 09:57:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-console.log('hello word');
+/*console.log('hello word');
 
-console.log('meuPrimeiroNome');
+console.log('meuPrimeiroNome');//canelcase
 
 
 var nome1 = '5';
 console.log(nome1);// a variavel nn pode comecar com numeros mas pode acabar com eles.
+
+
+//Tipos primitivos,Nan(undefined ou 0) +infinity(positivo) e -infinity(negativo).
+//se usa typeoff para saber qual tipo é.*/
+
+var float = 5.32;
+var int = '100';
+console.log( typeof int);
+console.log(typeof float);
+
+
